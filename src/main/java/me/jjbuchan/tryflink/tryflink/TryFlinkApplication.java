3e13a42c7b5038100453a70bda6fdad59befb751
James Buchan <james.buchan@rackspace.com>
2020-07-27 14:50:16 -0600

    Separate streams

--- src/main/java/me/jjbuchan/tryflink/tryflink/TryFlinkApplication.java
+++ src/main/java/me/jjbuchan/tryflink/tryflink/TryFlinkApplication.java
@@ -1,13 +1,17 @@
 package me.jjbuchan.tryflink.tryflink;
 
+import me.jjbuchan.tryflink.tryflink.service.DataProcessor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TryFlinkApplication {
 
-  public static void main(String[] args) {
+  public static void main(String[] args) throws Exception {
     SpringApplication.run(TryFlinkApplication.class, args);
+
+    DataProcessor.capitalize();
+    DataProcessor.join();
   }
 
 }
--- src/main/java/me/jjbuchan/tryflink/tryflink/service/DataProcessor.java
+++ src/main/java/me/jjbuchan/tryflink/tryflink/service/DataProcessor.java
@@ -5,6 +5,7 @@ import static me.jjbuchan.tryflink.tryflink.connector.Producers.createStringProd
 
 import lombok.extern.slf4j.Slf4j;
 import me.jjbuchan.tryflink.tryflink.operator.JoinString;
+import me.jjbuchan.tryflink.tryflink.operator.WordCapitalizer;
 import org.apache.flink.streaming.api.TimeCharacteristic;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
@@ -12,11 +13,9 @@ import org.apache.flink.streaming.api.windowing.assigners.TumblingEventTimeWindo
 import org.apache.flink.streaming.api.windowing.time.Time;
 import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer011;
 import org.apache.flink.streaming.connectors.kafka.FlinkKafkaProducer011;
-import org.springframework.boot.context.event.ApplicationReadyEvent;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Service;
 
-@Configuration
+@Service
 @Slf4j
 public class DataProcessor {
 
@@ -26,37 +25,59 @@ public class DataProcessor {
    *
    * @throws Exception
    */
-  @EventListener(ApplicationReadyEvent.class)
   public static void capitalize() throws Exception {
+    log.info("Initializing capitalize processor");
+
     String inputTopic = "inputTopic";
     String outputTopic = "outputTopic";
     String consumerGroup = "testGroup";
     String address = "localhost:9092";
 
     StreamExecutionEnvironment environment = StreamExecutionEnvironment.getExecutionEnvironment();
+
+    log.info("Creating consumer and starting from earliest offset");
     FlinkKafkaConsumer011<String> flinkKafkaConsumer = createStringConsumerForTopic(
         inputTopic, address, consumerGroup);
-
     flinkKafkaConsumer.setStartFromEarliest();
-    log.info("Created consumer and starting from earliest offset");
 
+    log.info("Creating producer");
+    FlinkKafkaProducer011<String> flinkKafkaProducer = createStringProducer(outputTopic, address);
+
+    log.info("Configuring sources");
     DataStream<String> stringInputStream = environment.addSource(flinkKafkaConsumer);
-
     DataStream<String> stringInputStream2 = environment.fromElements("asas", "sdsdsda", "TET", "34");
 
+    log.info("Processing kafka messages");
+    stringInputStream.union(stringInputStream2)
+        .map(new WordCapitalizer())
+        .addSink(flinkKafkaProducer);
 
+    environment.execute("Flink capitalize Example");
+  }
 
+  public static void join() throws Exception {
+    log.info("Initializing join processor");
 
+    String inputTopic = "inputTopic";
+    String outputTopic = "outputTopic";
+    String consumerGroup = "testGroup";
+    String address = "localhost:9092";
+
+    StreamExecutionEnvironment environment = StreamExecutionEnvironment.getExecutionEnvironment();
+
+    log.info("Creating consumer and starting from earliest offset");
+    FlinkKafkaConsumer011<String> flinkKafkaConsumer = createStringConsumerForTopic(
+        inputTopic, address, consumerGroup);
+    flinkKafkaConsumer.setStartFromEarliest();
+
+    log.info("Creating producer");
+    FlinkKafkaProducer011<String> flinkKafkaProducer = createStringProducer(outputTopic, address);
+
+    log.info("Configuring sources");
+    DataStream<String> stringInputStream = environment.addSource(flinkKafkaConsumer);
     DataStream<String> alertData = environment.fromElements("OK", "CRITICAL", "WARN");
 
-    FlinkKafkaProducer011<String> flinkKafkaProducer = createStringProducer(outputTopic, address);
-    log.info("Created producer");
-
     log.info("Processing kafka messages");
-//    stringInputStream.union(stringInputStream2)
-//        .map(new WordCapitalizer())
-//        .addSink(flinkKafkaProducer);
-
     stringInputStream.union(alertData)
         .keyBy(s -> s.charAt(0))
         .windowAll(TumblingEventTimeWindows.of(Time.seconds(10)))
@@ -64,6 +85,8 @@ public class DataProcessor {
         .addSink(flinkKafkaProducer);
 
     environment.setStreamTimeCharacteristic(TimeCharacteristic.IngestionTime);
-    environment.execute("Flink Kafka Example");
+    environment.execute("Flink join Example");
   }
+
+
 }
